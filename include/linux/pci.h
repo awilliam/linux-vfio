4b2390b9d8f6723a731a376d3cee190355c61889
Alex Williamson <alex.williamson@redhat.com>
2012-05-11 15:02:33 -0600

    pci: New pci_dma_quirk()
    Integrating IOMMU groups more closely into the driver core allows
    us to more easily work around DMA quirks.  The Ricoh multifunction
    controller is a favorite example of devices that are currently
    incompatible with IOMMU isolation as all the functions use the
    requestor ID of function 0 for DMA.  Passing this device into
    pci_dma_quirk returns the PCI device to use for DMA.  The IOMMU
    driver can then construct an IOMMU group including both devices.
    
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -1479,9 +1479,11 @@ enum pci_fixup_pass {
 
 #ifdef CONFIG_PCI_QUIRKS
 void pci_fixup_device(enum pci_fixup_pass pass, struct pci_dev *dev);
+struct pci_dev *pci_dma_quirk(struct pci_dev *dev);
 #else
 static inline void pci_fixup_device(enum pci_fixup_pass pass,
 				    struct pci_dev *dev) {}
+struct pci_dev *pci_dma_quirk(struct pci_dev *dev) { return dev }
 #endif
 
 void __iomem *pcim_iomap(struct pci_dev *pdev, int bar, unsigned long maxlen);
